bc9638f6ac894e0ddc514b3e0925f7419d27ed31
Garrett Krichbaum <thecornerbro@yahoo.com>
2018-09-17 15:49:26 -0400

    Allow users to search using regex (last resumes simply included schema models)

--- resumescanner/routes/resume.js
+++ resumescanner/routes/resume.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
+var db = mongoose.createConnection('mongodb://127.0.0.1:27017/resumes');
+
+// type term
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:type/:term', function(req, res, next) {
+  let query = {};
+  console.log(req.params.type + " " + req.params.term);
+  query[req.params.type] = new RegExp(req.params.term, 'i');
+  
+  db.model('resumes').find(query, function(err, result) {
+    res.json(result);
+
+    if (err) {
+      res.json(err);
+    }
+  });
 });
 
 module.exports = router;
