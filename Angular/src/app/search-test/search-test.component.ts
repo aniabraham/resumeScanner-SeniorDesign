51c88d960a3aeb25dcfc7950c50c11bb2f38a502
Garrett Krichbaum <thecornerbro@yahoo.com>
2018-10-17 16:11:08 -0400

    removed deprecated rxjs operators

--- Angular/src/app/search-test/search-test.component.ts
+++ Angular/src/app/search-test/search-test.component.ts
@@ -2,16 +2,6 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { ResumeData } from '../resume-data';
 import { SearchService } from '../search.service';
 
-import { of } from 'rxjs';
-import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
-
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/distinctUntilChanged';
-import 'rxjs/add/operator/switchMap';
-
 import 'jquery';
 declare let $: any;
 
@@ -28,7 +18,6 @@ export class SearchTestComponent implements OnInit {
 	@Output() select: EventEmitter<ResumeData> = new EventEmitter<ResumeData>();
 
 	data: ResumeData[];
-	private searchTerms = new Subject<string>();
 	private displayedData: ResumeData;
 	private type: string = 'name';
 
