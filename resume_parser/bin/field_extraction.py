ae12719df7b2bb8da48dc5be18c9924d3740bffb
ryubi <ryanubinger@ufl.edu>
2018-10-28 21:19:53 -0400

    Update field_extraction.py

--- resume_parser/bin/field_extraction.py
+++ resume_parser/bin/field_extraction.py
@@ -7,6 +7,7 @@ import lib
 
 EMAIL_REGEX = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
 PHONE_REGEX = r"1?\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*([0-9]{4})(\se?x?t?(\d*))?"
+GPA_REGEX = r"^[0-3](\.[0-9]{1,2})?$|^4(\.[0]{1,2})?$"
 
 
 def candidate_name_extractor(input_string, nlp):
