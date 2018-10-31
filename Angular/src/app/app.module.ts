c7a76e1bfa8f5435a791bac96b8f65a062680862
Garrett Krichbaum <thecornerbro@yahoo.com>
2018-10-31 13:50:07 -0400

    removed unused components

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -7,13 +7,10 @@ import { FormsModule } from '@angular/forms';
 import { AuthService } from './auth.service';
 
 import { AppComponent } from './app.component';
-import { TGamPdfViewerComponent } from './tgam-pdf-viewer/tgam-pdf-viewer.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import 'hammerjs';
 import { SearchTestComponent } from './search-test/search-test.component';
-import { TGamPDFComponent } from './tgam-pdf/tgam-pdf.component';
-import { TGamTableComponent } from './tgam-table/tgam-table.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ResumeViewerComponent } from './resume-viewer/resume-viewer.component';
@@ -23,10 +20,7 @@ import { SignupComponent } from './signup/signup.component';
 @NgModule({
 	declarations: [
 		AppComponent,
-		TGamPdfViewerComponent,
 		SearchTestComponent,
-		TGamPDFComponent,
-		TGamTableComponent,
 		LoginComponent,
 		ProfileComponent,
 		ResumeViewerComponent,
