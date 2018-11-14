80bbb1e9392f809b92155a1172d3905776688692
Garrett Krichbaum <thecornerbro@yahoo.com>
2018-11-14 16:13:23 -0500

    removed error handling : ^)

--- resumescanner/routes/mobile.js
+++ resumescanner/routes/mobile.js
@@ -72,84 +72,82 @@ router.post('/new', verifyToken, function(req, res, next) {
             multerUpload(req, res, err => {
 
                 if (err) {
-                    next(err);
+                    return res.json(err);
                 }
                 else {
-                    
-                }
-            });
+                    let newEnv = {};
+                    // copy current process environment variables in
+                    for (key in process.env) newEnv[key] = process.env[key];
 
-            let newEnv = {};
-            // copy current process environment variables in
-            for (key in process.env) newEnv[key] = process.env[key];
-
-            // overwrite necessary variables for tesseract execution
-            for (key in tesseractEnv) newEnv[key] = tesseractEnv[key];
-            const tesseract = spawn('python', 
-                [
-                    '/home/student/testing/shelltest.py',
-                    __dirname + '/../images',
-                    currentImage
-                ],
-                {
-                    shell: '/bin/bash',
-                    env: newEnv
-                });
-
-                tesseract.stderr.on('data', function(data) {
-                    let response = data.toString('utf8');
-                    response = response.split('\"').join('"');
-                    console.log(response);
-                });
-
-                tesseract.on('error', function(error) {
-                    console.log(error);
-                });
-                    
-                tesseract.on('exit', function (code, signal) {
-
-                    // if tesseract scanned successfully.
-                    if (code == 0) {
-                        let currentOutput = currentImage.split('.')[0] + '.txt';
-                        let parsEnv = {};
-                        for (key in process.env) parsEnv[key] = process.env[key];
-                        for (key in parserEnv) parsEnv[key] = parserEnv[key];
-                        const parser = spawn('python',
-                            [
-                                '/home/student/resume_parser/bin/main.py',
-                                '/home/student/finished/' + currentOutput
-                            ],
-                            {
-                            cwd: '/home/student/resume_parser/bin',
+                    // overwrite necessary variables for tesseract execution
+                    for (key in tesseractEnv) newEnv[key] = tesseractEnv[key];
+                    const tesseract = spawn('python', 
+                        [
+                            '/home/student/testing/shelltest.py',
+                            __dirname + '/../images',
+                            currentImage
+                        ],
+                        {
                             shell: '/bin/bash',
-                            env: parsEnv
-                            });
-                        
-                        parser.stdout.on('data', function(data) {
-                            let response = data.toString('utf8');
-                            response = response.replace('/\"/g','"');
-                            return res.json(JSON.parse(response));
+                            env: newEnv
                         });
-                
-                        parser.stderr.on('data', function(data) {
+
+                        tesseract.stderr.on('data', function(data) {
                             let response = data.toString('utf8');
                             response = response.split('\"').join('"');
                             console.log(response);
                         });
-                        parser.on('exit', function(code, signal) {
-                               
-                        });
-                        parser.on('error', function(error) {
+
+                        tesseract.on('error', function(error) {
                             console.log(error);
                         });
-                    }
+                            
+                        tesseract.on('exit', function (code, signal) {
 
-                    else {
-                        return res.json(
-                            "Tesseract failed to scan with error code: " + code
-                        );
+                            // if tesseract scanned successfully.
+                            if (code == 0) {
+                                let currentOutput = currentImage.split('.')[0] + '.txt';
+                                let parsEnv = {};
+                                for (key in process.env) parsEnv[key] = process.env[key];
+                                for (key in parserEnv) parsEnv[key] = parserEnv[key];
+                                const parser = spawn('python',
+                                    [
+                                        '/home/student/resume_parser/bin/main.py',
+                                        '/home/student/finished/' + currentOutput
+                                    ],
+                                    {
+                                    cwd: '/home/student/resume_parser/bin',
+                                    shell: '/bin/bash',
+                                    env: parsEnv
+                                    });
+                                
+                                parser.stdout.on('data', function(data) {
+                                    let response = data.toString('utf8');
+                                    response = response.replace('/\"/g','"');
+                                    return res.json(JSON.parse(response));
+                                });
+                        
+                                parser.stderr.on('data', function(data) {
+                                    let response = data.toString('utf8');
+                                    response = response.split('\"').join('"');
+                                    console.log(response);
+                                });
+                                parser.on('exit', function(code, signal) {
+                                    
+                                });
+                                parser.on('error', function(error) {
+                                    console.log(error);
+                                });
+                            }
+
+                            else {
+                                return res.json(
+                                    "Tesseract failed to scan with error code: " + code
+                                );
+                            }
+                        });
                     }
-                });
+            });
         }
     });
 });
